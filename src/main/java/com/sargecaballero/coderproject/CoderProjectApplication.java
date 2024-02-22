c97036c1d9c5797e23cde48a2370b704cf5cf104
Sergio R. Caballero <sargecaballero@gmail.com>
2024-02-22 17:56:00 -0600

    Initial commit

---
+++ src/main/java/com/sargecaballero/coderproject/CoderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sargecaballero.coderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sargecaballero/coderproject/CoderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sargecaballero.coderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
